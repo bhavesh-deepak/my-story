0c5a615d72a7c148493b898022b9090cca07e353
bhavesh <navandardeepak@gmail.com>
2022-03-26 14:24:39 +0530

    Add the story plot

---
+++ Story.js
@@ -0,0 +1,4 @@
+**story**
+once upon a time there was a  crocodile live   in water and a monkey on island on tree.a
+one day that crocodile come to island and rest under it . monkey offered some fruit to crocodile
+crocodile eat it and loved it.every day that crocodile come and eat fruit.they become best friends 
